72d64d10542e7b079fad5e3c0a4604462c63e2e5
fabian.coy <fabian.coy@grupomok.com>
2023-05-24 14:09:58 -0500

    creacion de los tres servicios

--- servicios/urls.py
+++ servicios/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import crearSiniestro
+from .views import crearSiniestro, ordendePago, crearTercero
 
 
 urlpatterns = [
     path('crearSiniestro', crearSiniestro ),
+    path('ordendePago', ordendePago ),
+    path('crearTercero', crearTercero ),
 ]
--- servicios/views.py
+++ servicios/views.py
@@ -3,31 +3,46 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 
-# # Create your views here.
-# @csrf_exempt
-# def crearSiniestrorest(request):
-#     if request.method == "post":
-#         print(request.body)
-
-#     return HttpResponse(f"Estas viendo los resultados de la pregunta numero ") 
-
-
-
-# @soap_view(['InputType'], ['OutputType'])
-# def crearSiniestro(request, input_data):
-#     # Lógica para procesar la solicitud SOAP y generar una respuesta
-#     return output_data
-
-
 @csrf_exempt
 def crearSiniestro(request):
     if request.method == 'POST':
         # Obtén el contenido XML de la solicitud
         xml_content = request.body
+        xml_respuesta = '<?xml version="1.0" encoding="utf-8"?><soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"><soapenv:Body><ns1:ClaimsModifyResponse xmlns:ns1="http://www.example.org/ServicioSiniestros2/"><Code xmlns="">0</Code><Message xmlns="">Siniestro Procesado. Numero: 26692 - Año Ejercicio: 126241/2021 - Poliza: 8057015900</Message><IdData xmlns="">90543568</IdData></ns1:ClaimsModifyResponse></soapenv:Body></soapenv:Envelope>'
 
         # Aquí puedes procesar el XML recibido y realizar cualquier lógica necesaria
         # En este ejemplo, simplemente devolvemos el mismo XML en la respuesta SOAP
-        response = f'<?xml version="1.0" encoding="UTF-8"?><soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"><soapenv:Body>{xml_content}</soapenv:Body></soapenv:Envelope>'
+        response = xml_respuesta
+        return HttpResponse(response, content_type='text/xml')
+
+    return HttpResponse(status=405)
+
+
+@csrf_exempt
+def ordendePago(request):
+    if request.method == 'POST':
+        # Obtén el contenido XML de la solicitud
+        xml_content = request.body
+        xml_respuesta = '<?xml version="1.0" encoding="utf-8"?><soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"><soapenv:Body><ns1:CreateRequestResponse xmlns:ns1="http://www.example.org/ServicioSiniestros2/"><Code xmlns="">0</Code><Message xmlns="">Siniestro Procesado. Numero: 26692 - nro_liquid_sis: 20210015743 - Año Ejercicio: 124007/2021 - Poliza: 8051525700</Message><IdData xmlns="">90541443447668</IdData></ns1:CreateRequestResponse></soapenv:Body></soapenv:Envelope>'
+
+        # Aquí puedes procesar el XML recibido y realizar cualquier lógica necesaria
+        # En este ejemplo, simplemente devolvemos el mismo XML en la respuesta SOAP
+        response = xml_respuesta
+        return HttpResponse(response, content_type='text/xml')
+
+    return HttpResponse(status=405)
+
+
+@csrf_exempt
+def crearTercero(request):
+    if request.method == 'POST':
+        # Obtén el contenido XML de la solicitud
+        xml_content = request.body
+        xml_respuesta = '''<?xml version='1.0' encoding='utf-8'?><soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"><soapenv:Body><ns1:ThirdProcessResponse xmlns:ns1="http://www.example.org/ServicioSiniestros2/"><Code>0</Code><Message>El tercero se ha creado Exitosamente</Message><IdData>11020482161</IdData></ns1:ThirdProcessResponse></soapenv:Body></soapenv:Envelope>'''
+
+        # Aquí puedes procesar el XML recibido y realizar cualquier lógica necesaria
+        # En este ejemplo, simplemente devolvemos el mismo XML en la respuesta SOAP
+        response = xml_respuesta
         return HttpResponse(response, content_type='text/xml')
 
     return HttpResponse(status=405)
